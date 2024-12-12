4776e7424661ff73e6b001bb95a0aaa4cafff09b
Manos Tsardoulias <etsardou@gmail.com>
2024-12-12 10:27:10 +0200

    Checked env cameras affections and pan tilt fixes

--- stream_simulator/controllers/env_devices/controller_camera.py
+++ stream_simulator/controllers/env_devices/controller_camera.py
@@ -6,14 +6,47 @@ import logging
 import threading
 import random
 import os
-import cv2
 import base64
-
-from colorama import Fore, Style
+import cv2
 
 from stream_simulator.base_classes import BaseThing
 
 class EnvCameraController(BaseThing):
+    """
+    EnvCameraController is a class that simulates a camera sensor in an environment. It inherits from BaseThing.
+    Attributes:
+        logger (logging.Logger): Logger instance for logging information.
+        info (dict): Dictionary containing camera information and configuration.
+        width (int): Width of the camera image.
+        height (int): Height of the camera image.
+        name (str): Name of the camera.
+        base_topic (str): Base topic for communication.
+        hz (int): Frequency at which the camera operates.
+        mode (str): Mode of operation (e.g., "mock", "simulation").
+        place (str): Place where the camera is located.
+        pose (dict): Pose of the camera.
+        derp_data_key (str): Key for raw data.
+        range (int): Range of the camera.
+        fov (int): Field of view of the camera.
+        env_properties (dict): Environmental properties.
+        host (str): Host information.
+        images (dict): Dictionary of image resources.
+    Methods:
+        __init__(conf=None, package=None):
+            Initializes the EnvCameraController with the given configuration and package.
+        set_communication_layer(package):
+            Sets up the communication layer for the camera.
+        sensor_read():
+            Reads data from the sensor and publishes it.
+        enable_callback(message):
+            Callback function to enable the camera.
+        disable_callback(message):
+            Callback function to disable the camera.
+        start():
+            Starts the camera and its RPC servers.
+        stop():
+            Stops the camera and its RPC servers.
+    """
     def __init__(self,
                  conf = None,
                  package = None
@@ -127,17 +160,16 @@ class EnvCameraController(BaseThing):
             data = None
 
             if self.mode == "mock":
-                im = cv2.imread(dirname + '/resources/all.png')
-                im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-                image = cv2.resize(im, dsize=(width, height))
-                _, buffer = cv2.imencode('.jpg', image)
-                data = base64.b64encode(buffer).decode()
+                with open(dirname + '/resources/all.png', "rb") as f:
+                    fdata = f.read()
+                    b64 = base64.b64encode(fdata)
+                    data = b64.decode()
             elif self.mode == "simulation":
                 # Ask tf for proximity sound sources or humans
                 res = self.tf_affection_rpc.call({
                     'name': self.name
                 })
-                print("Affections: ", res)
+                # print("Affections: ", res)
 
                 # Get the closest:
                 clos = None
@@ -152,12 +184,13 @@ class EnvCameraController(BaseThing):
                 else:
                     cl_type = res[clos]['type']
 
+                # print("Closest: ", clos, clos_d, cl_type)
+
                 # types: qr, barcode, color, text, human
                 if cl_type is None:
-                    img = "all.png"
+                    img = "all.jpg"
                 elif cl_type == "human":
                     img = random.choice(["face.jpg", "face_inverted.jpg"])
-
                 elif cl_type == "qr":
                     import qrcode
                     try:
@@ -166,10 +199,8 @@ class EnvCameraController(BaseThing):
                         self.logger.error(f"QR creator could not produce string: {res[clos]['info']['message']} or qrcode library is not installed: {str(e)}")
                     im.save(dirname + "/resources/qr_tmp.png")
                     img = "qr_tmp.png"
-
                 elif cl_type == "barcode":
                     img = "barcode.jpg"
-
                 elif cl_type == 'color':
                     import numpy as np
                     img = 'col_tmp.png'
@@ -179,30 +210,38 @@ class EnvCameraController(BaseThing):
                         res[clos]['info']["g"],
                         res[clos]['info']["r"]
                     )
-                    cv2.imwrite(dirname + "/resources/" + img, tmp)
-
+                    cv2.imwrite(dirname + "/resources/" + img, tmp) # pylint: disable=no-member
                 elif cl_type == "text":
                     import numpy as np
                     img = 'txt_temp.png'
                     try:
-                        from PIL import Image, ImageDraw, ImageFont, ImageFilter
-                        im  =  Image.new ( "RGB", (width,height), (255, 255, 255) )
-                        draw  =  ImageDraw.Draw ( im )
+                        image = np.zeros((height, width, 3), dtype=np.uint8)
+                        # Define the text and its properties
                         final_text = res[clos]['info']["text"]
-                        final_text = [final_text[i:i+30] for i in range(0, len(final_text), 30)]
+                        font = cv2.FONT_HERSHEY_SIMPLEX # pylint: disable=no-member
+                        font_scale = 2
+                        color = (255, 255, 255)  # White color
+                        thickness = 3
 
-                        start_coord = 30
-                        for i in final_text:
-                            draw.text (
-                                (10, start_coord),
-                                i,
-                                font=ImageFont.truetype("DejaVuSans.ttf", 36),
-                                fill=(0,0,0)
-                            )
-                            start_coord += 40
-                        im.save(dirname + "/resources/" + img)
+                        # Calculate text size and position to center it
+                        # Also adjust to text size
+                        x = -1
+                        while x < 0:
+                            (text_width, text_height), _ = cv2.getTextSize(final_text, font, font_scale, thickness) # pylint: disable=no-member
+                            x = (width - text_width) // 2
+                            y = (height + text_height) // 2
+                            if x < 0:
+                                font_scale -= 0.1
+                                thickness = thickness - 1 if thickness > 1 else 1
+
+                        # Draw the text on the image
+                        cv2.putText(image, final_text, (x, y), font, font_scale, color, thickness, lineType=cv2.LINE_AA) # pylint: disable=no-member
+
+                        # Save the image to a file
+                        cv2.imwrite(dirname + "/resources/" + img, image) # pylint: disable=no-member
+
                     except Exception as e:
-                        self.logger.error(f"CameraController: Error with text image generation: {str(e)}")
+                        self.logger.error("CameraController: Error with text image generation: %s", str(e))
 
                 with open(dirname + "/resources/" + img, "rb") as f:
                     fdata = f.read()
@@ -221,7 +260,7 @@ class EnvCameraController(BaseThing):
                         },
                         "timestamp": time.time()
                     })
-
+                
     def enable_callback(self, message):
         self.info["enabled"] = True
         self.sensor_read_thread = threading.Thread(target = self.sensor_read)
